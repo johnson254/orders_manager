9ed776f1063bcbf8960f102f9af1f0fd057da9ed
johnson254 <wambirujohnson@gmail.com>
2023-03-04 12:54:10 -0800

    created models for delivery notes

--- delivery_notes/apps.py
+++ delivery_notes/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class DeliveryNotesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'delivery_notes'
--- delivery_notes/models.py
+++ delivery_notes/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
 class DeliveryNote(models.Model):
-    delivery_note_number = models.CharField(max_length=255)
+    delivery_note_number = models.CharField(max_length=50, unique=True)
     date = models.DateField()
-    item_name = models.CharField(max_length=255)
-    quantity = models.PositiveIntegerField()
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    invoice_number = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='delivery_notes')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.delivery_note_number
+
+    def save(self, *args, **kwargs):
+        super(DeliveryNote, self).save(*args, **kwargs)
+
+    @staticmethod
+    def get_all_delivery_notes():
+        return DeliveryNote.objects.all()
+
+    @staticmethod
+    def search_delivery_note_by_number(delivery_note_number):
+        return DeliveryNote.objects.filter(delivery_note_number__icontains=delivery_note_number)
